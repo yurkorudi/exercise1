7b5e6a1c01638118870f4b81605a757641961b7e
yurkorudi <yurkorudi@gmail.com>
2022-11-15 14:44:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from PIL import Image
 
 x = requests.get('https://www.google.com.ua/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png').content
-with open('google_image.jpg', 'wb') as handler:
-    handler.write(x)
+with open('google_image.jpg', 'wb') as image_gg:
+    image_gg.write(x)
 print('image downloaded')
 img = Image.open('google_image.jpg')
 img.show()
